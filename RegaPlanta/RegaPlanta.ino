3066a7afd4a1f0636da55aa7021e7d490ab4d869
Alexandre Venito <avenito@yahoo.com.br>
2025-09-07 20:06:32 +0200

    Update ino

--- RegaPlanta/RegaPlanta.ino
+++ RegaPlanta/RegaPlanta.ino
@@ -1,6 +1,7 @@
 #include "WiFi.h"
 #include "PubSubClient.h"
 #include "DHT.h"
+#include "config.h"
 
 #define DHTPIN 0
 #define DHTTYPE DHT11
@@ -9,21 +10,7 @@
 
 DHT dht(DHTPIN, DHTTYPE);
 
-const char* ssid = "VIRUS2X";
-const char* password = "60120036273529369257";
 const int pump = 10;
-const char* ntpServer1 = "ntp1.informatik.uni-erlangen.de";
-const char* ntpServer2 = "ptbtime1.ptb.de";
-const char* ntpServer3 = "ptbtime2.ptb.de";
-
-
-//const char* mqtt_server = "maqiatto.com"; // IP do seu broker MQTT (ex: Mosquitto)
-//const char* mqtt_user = "tissianorodri@gmail.com";  // se não tiver auth, deixe em branco
-//const char* mqtt_pass = "vt260957";
-const char* mqtt_server = "avenito.ddns.net";  // IP do seu broker MQTT (ex: Mosquitto)
-const char* mqtt_user = "user01";              // se não tiver auth, deixe em branco
-const char* mqtt_pass = "123456";
-const int mqtt_port = 1883;
 
 struct tm timeinfo;
 int numReconn = -1;
---
+++ RegaPlanta/config.h
@@ -0,0 +1,15 @@
+const char* ssid = "VIRUS2X";
+const char* password = "60120036273529369257";
+
+const char* ntpServer1 = "ntp1.informatik.uni-erlangen.de";
+const char* ntpServer2 = "ptbtime1.ptb.de";
+const char* ntpServer3 = "ptbtime2.ptb.de";
+
+//const char* mqtt_server = "maqiatto.com"; // IP do seu broker MQTT (ex: Mosquitto)
+//const char* mqtt_user = "tissianorodri@gmail.com";  // se não tiver auth, deixe em branco
+//const char* mqtt_pass = "vt260957";
+const char* mqtt_server = "avenito.ddns.net";  // IP do seu broker MQTT (ex: Mosquitto)
+const char* mqtt_user = "user01";              // se não tiver auth, deixe em branco
+const char* mqtt_pass = "123456";
+const int mqtt_port = 1883;
+
